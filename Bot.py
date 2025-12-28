6ab5a628362f6e89b423d4272d40b195a2f41ae2
unknown <felipebolt2007@gmail.com>
2025-12-28 17:23:07 -0300

    A

---
+++ Bot.py
@@ -0,0 +1,21 @@
+import discord
+from discord.ext import commands
+
+import os
+from dotenv import load_dotenv
+
+# ? Commita com o TOKEN na env
+
+load_dotenv()  # abre o .env e joga as variaveis pro ambiente
+TOKEN = os.getenv("DISCORD_TOKEN") # Aqui pega o valor da variavel que eu quero
+
+# Permissões que o Bot precisa(ele tem todas as permissões)
+
+intents = discord.Intents.all()
+bot = commands.Bot(".", intents=intents) # Esse ponto é como acessa o bot
+
+@bot.event
+async def on_ready():
+    print("O Teste Chegou😎🔥")
+
+bot.run(TOKEN)
