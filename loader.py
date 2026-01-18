9e4fb06fcdd69e43877da6f6cf6629025b4bebd1
unknown <felipebolt2007@gmail.com>
2026-01-18 11:52:04 -0300

    dsa

--- loader.py
+++ loader.py
@@ -2,12 +2,12 @@ import json
 import os
 
 def load_db():
-    if os.path.exists("Banco/Data.json"):
-        with open("Banco/Data.json", "r", encoding="utf-8") as f:
+    if os.path.exists("Json/Data.json"):
+        with open("Json/Data.json", "r", encoding="utf-8") as f:
             return json.load(f)
     return []
 
 def save_db(dados):
-    with open("Banco/Data.json", "w", encoding="utf-8") as f:
+    with open("Json/Data.json", "w", encoding="utf-8") as f:
         json.dump(dados, f, indent=4, ensure_ascii=False)
 
